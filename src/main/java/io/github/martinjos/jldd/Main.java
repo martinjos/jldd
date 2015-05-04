58380e95ab6bb067dcab630c66423d2ae63b7f53
Martin Sidaway <msidaway@gmail.com>
2015-05-04 17:43:31 +0100

    Decay gracefully.

--- src/main/java/io/github/martinjos/jldd/Main.java
+++ src/main/java/io/github/martinjos/jldd/Main.java
@@ -67,9 +67,13 @@ public class Main {
         }
     }
 
-    private static void saveClassLookups(Map<String,String> map, String filename)
-    throws IOException {
-        ZipFile f = new ZipFile(filename);
+    private static void saveClassLookups(Map<String,String> map, String filename) {
+        ZipFile f = null;
+        try {
+            f = new ZipFile(filename);
+        } catch (IOException e) {
+            return;
+        }
         for (ZipEntry ent : new EnumerationIterable<ZipEntry>(f.entries())) {
             if (!ent.isDirectory()) {
                 String name = ent.getName();
@@ -88,80 +92,91 @@ public class Main {
         }
     }
 
-    private static Set<String> getTypesUsed(String filename)
+    private static void processZipEntry(Set<String> types, ZipEntry ent, ZipFile f, String filename)
     throws IOException {
-        ZipFile f = new ZipFile(filename);
+        //System.out.println("Processing " + ent);
+        DataInputStream dis = new DataInputStream(f.getInputStream(ent));
+        dis.skipBytes(8);
+        int cpCount = dis.readUnsignedShort();
+        Map<Integer,String> utf8 = new HashMap<Integer,String>();
+        Set<Integer> validIdxs = new HashSet<Integer>();
+        Set<Integer> validIdxsBare = new HashSet<Integer>();
+        for (int i = 1; i < cpCount; i++) {
+            int typeTag = dis.readUnsignedByte();
+            ConstantType type = ConstantType.byTag(typeTag);
+            if (type == ConstantType.Utf8) {
+                //System.out.println("Got " + type);
+                int utf8len = dis.readUnsignedShort();
+                byte[] bytes = new byte[utf8len];
+                dis.readFully(bytes);
+                String str = null;
+                try {
+                    str = new String(bytes, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    throw new RuntimeException(e);
+                }
+                utf8.put(i, str);
+            } else if (type == null) {
+                //System.out.println("Error: got null tag in "+filename+"/"+ent.getName()+" (tag = "+typeTag+"): ending at index "+i+"; cpCount = " + cpCount);
+                break;
+            } else if (type == ConstantType.Class) {
+                // Bare class names, apart from array classes
+                validIdxsBare.add(dis.readUnsignedShort());
+            } else if (type == ConstantType.MethodType) {
+                // I assume this is in standard form
+                validIdxs.add(dis.readUnsignedShort());
+            } else if (type == ConstantType.NameAndType) {
+                // Standard form
+                dis.skipBytes(2);
+                validIdxs.add(dis.readUnsignedShort());
+            } else {
+                //if (type != ConstantType.String && type != ConstantType.Methodref && type != ConstantType.Fieldref && type != ConstantType.InterfaceMethodref) {
+                    //System.out.println("Got " + type);
+                //}
+                if (type == ConstantType.Long || type == ConstantType.Double) {
+                    i++;
+                }
+                dis.skipBytes(type.getLength());
+            }
+        }
+        //System.out.println("utf8.size() == " + utf8.size());
+        //System.out.println("validIdxs.size() == " + validIdxs.size());
+        //System.out.println("validIdxsBare.size() == " + validIdxsBare.size());
+        for (int idx : validIdxs) {
+            if (!utf8.containsKey(idx)) {
+                System.err.println("ERROR: Index is not valid: " + idx);
+                continue;
+            }
+            types.add(utf8.get(idx));
+        }
+        for (int idx : validIdxsBare) {
+            if (!utf8.containsKey(idx)) {
+                System.err.println("ERROR: Index is not valid: " + idx);
+                continue;
+            }
+            String typeStr = utf8.get(idx);
+            if (!typeStr.startsWith("[")) {
+                // Ensure that all class names can be extracted in the
+                // same way
+                typeStr = "#L" + typeStr + ";";
+            }
+            types.add(typeStr);
+        }
+    }
+
+    private static Set<String> getTypesUsed(String filename) {
         Set<String> types = new TreeSet<String>();
+        ZipFile f = null;
+        try {
+            f = new ZipFile(filename);
+        } catch (IOException e) {
+            return types;
+        }
         for (ZipEntry ent : new EnumerationIterable<ZipEntry>(f.entries())) {
             if (!ent.isDirectory() && ent.getName().toLowerCase().endsWith(".class")) {
-                //System.out.println("Processing " + ent);
-                DataInputStream dis = new DataInputStream(f.getInputStream(ent));
-                dis.skipBytes(8);
-                int cpCount = dis.readUnsignedShort();
-                Map<Integer,String> utf8 = new HashMap<Integer,String>();
-                Set<Integer> validIdxs = new HashSet<Integer>();
-                Set<Integer> validIdxsBare = new HashSet<Integer>();
-                for (int i = 1; i < cpCount; i++) {
-                    int typeTag = dis.readUnsignedByte();
-                    ConstantType type = ConstantType.byTag(typeTag);
-                    if (type == ConstantType.Utf8) {
-                        //System.out.println("Got " + type);
-                        int utf8len = dis.readUnsignedShort();
-                        byte[] bytes = new byte[utf8len];
-                        dis.readFully(bytes);
-                        String str = null;
-                        try {
-                            str = new String(bytes, "UTF-8");
-                        } catch (UnsupportedEncodingException e) {
-                            throw new RuntimeException(e);
-                        }
-                        utf8.put(i, str);
-                    } else if (type == null) {
-                        //System.out.println("Error: got null tag in "+filename+"/"+ent.getName()+" (tag = "+typeTag+"): ending at index "+i+"; cpCount = " + cpCount);
-                        break;
-                    } else if (type == ConstantType.Class) {
-                        // Bare class names, apart from array classes
-                        validIdxsBare.add(dis.readUnsignedShort());
-                    } else if (type == ConstantType.MethodType) {
-                        // I assume this is in standard form
-                        validIdxs.add(dis.readUnsignedShort());
-                    } else if (type == ConstantType.NameAndType) {
-                        // Standard form
-                        dis.skipBytes(2);
-                        validIdxs.add(dis.readUnsignedShort());
-                    } else {
-                        //if (type != ConstantType.String && type != ConstantType.Methodref && type != ConstantType.Fieldref && type != ConstantType.InterfaceMethodref) {
-                            //System.out.println("Got " + type);
-                        //}
-                        if (type == ConstantType.Long || type == ConstantType.Double) {
-                            i++;
-                        }
-                        dis.skipBytes(type.getLength());
-                    }
-                }
-                //System.out.println("utf8.size() == " + utf8.size());
-                //System.out.println("validIdxs.size() == " + validIdxs.size());
-                //System.out.println("validIdxsBare.size() == " + validIdxsBare.size());
-                for (int idx : validIdxs) {
-                    if (!utf8.containsKey(idx)) {
-                        System.err.println("ERROR: Index is not valid: " + idx);
-                        continue;
-                    }
-                    types.add(utf8.get(idx));
-                }
-                for (int idx : validIdxsBare) {
-                    if (!utf8.containsKey(idx)) {
-                        System.err.println("ERROR: Index is not valid: " + idx);
-                        continue;
-                    }
-                    String typeStr = utf8.get(idx);
-                    if (!typeStr.startsWith("[")) {
-                        // Ensure that all class names can be extracted in the
-                        // same way
-                        typeStr = "#L" + typeStr + ";";
-                    }
-                    types.add(typeStr);
-                }
+                try {
+                    processZipEntry(types, ent, f, filename);
+                } catch (IOException e) {}
             }
         }
         return types;
@@ -182,8 +197,7 @@ public class Main {
         }
     }
 
-    private static Set<String> getRefsUsed(String filename, Map<String,String> lookups)
-    throws IOException {
+    private static Set<String> getRefsUsed(String filename, Map<String,String> lookups) {
         Set<String> refs = new TreeSet<String>();
         for (String type : getTypesUsed(filename)) {
             //System.out.println(type);
@@ -193,8 +207,7 @@ public class Main {
     }
 
     private static void printTree(String filename, Map<String,String> lookups,
-                                  Set<String> visited, int level, String rt)
-    throws IOException {
+                                  Set<String> visited, int level, String rt) {
         for (int i = 0; i < level; i++) {
             System.out.print("  ");
         }
@@ -212,10 +225,20 @@ public class Main {
         visited.remove(filename);
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("ERROR: no JAR file given");
+            return;
+        }
         Map<String,String> lookups = new TreeMap<String,String>();
         saveClassLookups(lookups, args[0]);
-        String[] cp = args[1].split(File.pathSeparator);
+        String[] cp = null;
+        if (args.length >= 2) {
+            cp = args[1].split(File.pathSeparator);
+        } else {
+            cp = System.getProperty("java.class.path")
+                       .split(System.getProperty("path.separator"));
+        }
         for (String cpstr : cp) {
             saveClassLookups(lookups, cpstr);
         }
